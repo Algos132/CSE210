3012c852528da47d778cf3262e90c9c783e0b549
Algos132 <mikeph132@gmail.com>
2025-09-10 17:38:36 -0600

    Completed Exercise2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,41 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("What is your percentage? ");
+        string ValueFromUser = Console.ReadLine();
+
+        int Percentage = int.Parse(ValueFromUser);
+        int PercentageSign = Percentage / 10;
+
+        if (Percentage >= 90)
+        {
+            Console.Write("Your grade is an A");
+        }
+        else if (Percentage >= 80)
+        {
+            Console.Write("Your grade is a B");
+        }
+        else if (Percentage >= 70)
+        {
+            Console.Write("Your grade is a C");
+        }
+        else if (Percentage >= 60)
+        {
+            Console.Write("Your grade is a D");
+        }
+        else
+        {
+            Console.Write("Your grade is a F");
+        }
+
+        if (Percentage >= 60)
+        {
+            Console.WriteLine(". Congradulations! You're passing!");
+        }
+        else
+        {
+            Console.WriteLine(". Sorry. You'll do better next time!");
+        }
     }
 }
