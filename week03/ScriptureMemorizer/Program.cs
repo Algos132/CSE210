daaceba79ea80ad92262a248e3b2540098ee399f
Algos132 <mikeph132@gmail.com>
2025-09-26 22:32:36 -0600

    Quick fix for scripture in ScriptureMemorizer

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -7,7 +7,7 @@ class Program
     {
         //Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
         //Reference reference = new Reference("Ether", 12, 27);
-        Reference reference = new Reference("Ether", 12, 27, 29);
+        Reference reference = new Reference("Ether", 12, 27);
         Scripture scripture = new Scripture("And if men come unto me I will show unto them their weakness. I give unto men weakness that they may be humble; and my grace is sufficient for all men that humble themselves before me; for if they humble themselves before me, and have faith in me, then will I make weak things become strong unto them.");
         bool running = true;
         while (running == true)
