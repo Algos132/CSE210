59c0619004c0a87b1b89988a8aa00fb45f98ec46
Algos132 <mikeph132@gmail.com>
2025-11-01 14:34:18 -0600

    Redid Exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,10 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        //Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
         Random randomGenerator = new Random();
         int number = randomGenerator.Next(1, 100);
-        int guess = 0;
+        int guess;
         int times = 0;
 
         Console.WriteLine("Guess a number between 1 and 100: ");
@@ -16,8 +17,8 @@ class Program
         {
             Console.Write("Enter your guess: ");
 
-            string ValueFromUser = Console.ReadLine();
-            guess = int.Parse(ValueFromUser);
+            string valueFromUser = Console.ReadLine();
+            guess = int.Parse(valueFromUser);
             ++times;
 
             if (guess > number)
